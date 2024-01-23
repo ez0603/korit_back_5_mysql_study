1ca4f1ed72f0e9a432586a581d5fee03e67081c5
ez0603 <dlwldjs3132@naver.com>
2024-01-23 17:56:15 +0900

    -

---
+++ book_insert.sql
@@ -0,0 +1,33 @@
+set @book_name = '혼자 공부하는 SQL';
+set @author = '우재남';
+set @publisher = '한빛미디어';
+set @new_author_id = 0;
+set @new_publisher_id = 0;
+
+insert into author_tb
+values (0, @author);
+
+select
+	max(author_id) into @new_author_id # author_id 중 제일 큰 값 = 맨 마지막에 넣은값
+from
+	author_tb;
+    
+select @new_author_id;
+
+insert into publisher_tb
+values (0, @publisher);
+
+select
+	max(publisher_id) into @new_publisher_id 
+from
+	publisher_tb;
+
+insert into book_tb
+values (0, @book_name, @new_author_id, @new_publisher_id);
+
+select
+	*
+from
+	book_tb
+order by
+	book_id desc;
---
+++ book_search.sql
@@ -0,0 +1,19 @@
+# 책id, 책이름, 저자, 출판사
+
+select
+	bt.book_name,
+	at.author_name,
+    pt.publisher_name
+from  
+	book_tb bt
+    left outer join author_tb at on (at.author_id = bt.author_id)
+    left outer join publisher_tb pt on (pt.publisher_id = bt.publisher_id)
+
+where
+	bt.book_name like '%?%';
+	
+    
+    
+
+    
+
